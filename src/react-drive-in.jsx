bc973f1cb939fb109fdcc0deca9bdcbf0b06da04
Michael Shick <m@shick.us>
2015-09-30 18:15:32 -0400

    code style cleanup, fixed bug in drive-in core

--- src/react-drive-in.jsx
+++ src/react-drive-in.jsx
@@ -1,191 +1,188 @@
-import React from 'react';
-import DriveIn from 'drive-in';
+import React from "react";
+import DriveIn from "drive-in";
 
 class ReactDriveIn extends React.Component {
 
-    constructor(props) {
+  constructor(props) {
 
-        super(props);
+    super(props);
 
-        this.state = {
-            className: 'drive-in',
-            mute: true,
-            loop: true,
-            loopPaylistItems: false,
-            slideshow: false,
-            volume: 0.5,
-            onTimeFrequency: 500
-        };
+    this.state = {
+      className: props.className,
+      mute: props.mute,
+      loop: props.loop,
+      loopPaylistItems: props.loopPlaylistItems,
+      slideshow: props.slideshow,
+      volume: props.volume,
+      onTimeFrequency: props.onTimeFrequency
+    };
+
+    this.setPlaying = this.setPlaying.bind(this);
+    this.setPause = this.setPause.bind(this);
+    this.setLoading = this.setLoading.bind(this);
+    this.setCanPlay = this.setCanPlay.bind(this);
+  }
+
+  getMedia() {
+    return this.refs.media.getDOMNode();
+  }
+
+  getPlaylist() {
+    return this.state.playlist;
+  }
+
+  setPlaying(currentItem) {
+    this.setState({ playing: true, currentItem });
+    if (this.props.onPlaying) {
+      this.props.onPlaying(currentItem);
+    }
+  }
+
+  setPause() {
+    this.setState({ playing: false });
+    if (this.props.onPause) {
+      this.props.onPause();
+    }
+  }
+
+  setLoading() {
+    this.setState({ canPlay: false });
+  }
+
+  setCanPlay() {
+    this.setState({ canPlay: true });
+    if (this.props.onCanPlay) {
+      this.props.onCanPlay();
+    }
+  }
+
+  componentWillMount() {
+    this.DI = new DriveIn();
+    this.DI.on("media.playing", this.setPlaying);
+    this.DI.on("media.pause", this.setPause);
+    this.DI.on("media.loading", this.setLoading);
+    this.DI.on("media.canplay", this.setCanPlay);
+  }
+
+  componentDidMount() {
+
+    let playlist;
+
+    this.DI.init({
+      el: this.getMedia(),
+      slideshow: this.props.slideshow
+    });
+
+    const options = {
+      mute: this.props.mute,
+      loop: this.props.loop,
+      loopPlaylistItems: this.props.loopPlaylistItems,
+      poster: this.props.poster,
+      isTouch: this.props.isTouch
+    };
+
+    if (this.props.showPlaylist) {
+      playlist = this.DI.showPlaylist(this.props.showPlaylist, options);
+    } else {
+      playlist = this.DI.show(this.props.show, options);
     }
 
-    getMedia() {
-        return this.refs.media.getDOMNode();
+    if (this.props.onTime) {
+      this.intervalId = window.setInterval(() => {
+        this.props.onTime(this.DI.currentTime());
+      }, this.props.onTimeFrequency);
     }
 
-    getPlaylist() {
-        return this.state.playlist;
+    if (this.props.playbackRate) {
+      this.playbackRate(this.props.playbackRate);
     }
 
-    setPlaying(currentItem) {
-        this.setState({
-            playing: true,
-            currentItem: currentItem
-        });
+    this.setState({ initalized: true, playlist });
+  }
 
-        if (this.props.onPlaying) {
-            this.props.onPlaying(currentItem);
-        }
+  componentWillUnmount() {
+    if (this.intervalId) {
+      window.clearInterval(this.intervalId);
     }
 
-    setPause() {
-        this.setState({ playing: false });
-        if (this.props.onPause) {
-            this.props.onPause();
-        }
-    }
+    this.DI.removeAllListeners();
+    this.DI.close();
+    delete this.DI;
+  }
 
-    setLoading() {
-        this.setState({ canPlay: false });
-    }
+  play(itemNum) {
+    this.DI.play(itemNum);
+  }
 
-    setCanPlay() {
-        this.setState({ canPlay: true });
-        if (this.props.onCanPlay) {
-            this.props.onCanPlay();
-        }
-    }
+  pause() {
+    this.DI.pause();
+  }
 
-    componentWillMount() {
-        this.DI = new DriveIn();
-    }
+  mute() {
+    this.DI.setVolume(0);
+    this.setState({ mute: true });
+  }
 
-    componentDidMount() {
-        var DI = this.DI,
-            options,
-            playlist;
+  unmute() {
+    this.DI.setVolume(this.props.volume);
+    this.setState({ mute: false });
+  }
 
-        DI.init({ el: this.getMedia(), slideshow: this.props.slideshow });
+  playbackRate(rate) {
+    rate = rate || 1.0;
+    this.DI.setPlaybackRate(rate);
+    this.setState({ playbackRate: rate });
+  }
 
-        options = {
-            mute: this.props.mute,
-            loop: this.props.loop,
-            loopPlaylistItems: this.props.loopPlaylistItems,
-            poster: this.props.poster,
-            isTouch: this.props.isTouch
-        };
+  seekTo(time) {
+    this.DI.seekTo(time);
+  }
 
-        if (this.props.showPlaylist) {
-            playlist = DI.showPlaylist(this.props.showPlaylist, options);
-        } else {
-            playlist = DI.show(this.props.show, options);
-        }
+  duration() {
+    return this.DI.duration();
+  }
 
-        DI.on('media.playing', (currentItem) => {
-            this.setPlaying(currentItem);
-        });
-        DI.on('media.pause', () => { this.setPause(); });
-        DI.on('media.loading', () => { this.setLoading(); });
-        DI.on('media.canplay', () => { this.setCanPlay(); });
-
-        if (this.props.onTime) {
-            this.intervalId = window.setInterval(() => {
-                var currTime = DI.currentTime();
-                this.props.onTime(currTime);
-            }, this.props.onTimeFrequency);
-        }
-
-        if (this.props.playbackRate) {
-            this.playbackRate(this.props.playbackRate);
-        }
-
-        this.setState({
-            mute: this.props.mute,
-            playlist: playlist,
-            initalized: true
-        });
-    }
-
-    componentWillUnmount() {
-        if (this.intervalId) {
-            window.clearInterval(this.intervalId);
-        }
-
-        this.DI.removeAllListeners();
-        this.DI.close();
-        delete this.DI;
-    }
-
-    play(itemNum) {
-        this.DI.play(itemNum);
-    }
-
-    pause() {
-        this.DI.pause();
-    }
-
-    mute() {
-        this.DI.setVolume(0);
-        this.setState({ mute: true });
-    }
-
-    unmute() {
-        this.DI.setVolume(this.props.volume);
-        this.setState({ mute: false });
-    }
-
-    playbackRate(rate) {
-        rate = rate || 1.0;
-        this.DI.setPlaybackRate(rate);
-        this.setState({ playbackRate: rate });
-    }
-
-    seekTo(time) {
-        this.DI.seekTo(time);
-    }
-
-    duration() {
-        return this.DI.duration();
-    }
-
-    render() {
-        return (
-          <div ref="wrap" className={this.props.className + ' drive-in-wrap'}>
-            <div ref="media" className="drive-in-media"></div>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div ref="wrap" className={this.props.className + " drive-in-wrap"}>
+        <div ref="media" className="drive-in-media"></div>
+      </div>
+    );
+  }
 }
 
-ReactDriveIn.displayName = 'DriveIn';
+ReactDriveIn.displayName = "DriveIn";
 
 ReactDriveIn.propTypes = {
-    show: React.PropTypes.oneOfType([
-        React.PropTypes.string,
-        React.PropTypes.array
-        ]),
-    showPlaylist: React.PropTypes.oneOfType([React.PropTypes.array]),
-    poster: React.PropTypes.string,
-    mute: React.PropTypes.bool,
-    loop: React.PropTypes.bool,
-    loopPlaylistItems: React.PropTypes.bool,
-    playbackRate: React.PropTypes.number,
-    slideshow: React.PropTypes.bool,
-    onPlaying: React.PropTypes.func,
-    onPause: React.PropTypes.func,
-    onTime: React.PropTypes.func,
-    onTimeFrequency: React.PropTypes.number,
-    onCanPlay: React.PropTypes.func,
-    isTouch: React.PropTypes.func
+  show: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.array
+  ]),
+  showPlaylist: React.PropTypes.oneOfType([React.PropTypes.array]),
+  poster: React.PropTypes.string,
+  mute: React.PropTypes.bool,
+  loop: React.PropTypes.bool,
+  loopPlaylistItems: React.PropTypes.bool,
+  playbackRate: React.PropTypes.number,
+  slideshow: React.PropTypes.bool,
+  onPlaying: React.PropTypes.func,
+  onPause: React.PropTypes.func,
+  onTime: React.PropTypes.func,
+  onTimeFrequency: React.PropTypes.number,
+  onCanPlay: React.PropTypes.func,
+  isTouch: React.PropTypes.func,
+  volume: React.PropTypes.number,
+  className: React.PropTypes.string
 };
 
 ReactDriveIn.defaultProps = {
-    className: 'drive-in',
-    mute: true,
-    loop: true,
-    loopPaylistItems: false,
-    slideshow: false,
-    volume: 0.5,
-    onTimeFrequency: 500
+  className: "drive-in",
+  mute: true,
+  loop: true,
+  loopPaylistItems: false,
+  slideshow: false,
+  volume: 0.5,
+  onTimeFrequency: 500
 };
 
 export default ReactDriveIn;
